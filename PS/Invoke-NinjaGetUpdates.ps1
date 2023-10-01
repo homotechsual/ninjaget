3ff31f07dbfe277eb6b7efe0c4b6817b786e8348
Mikey O'Toole <mikey@homotechsual.dev>
2023-10-01 13:49:50 +0100

    Fix incorrect import of updates file.

--- PS/Invoke-NinjaGetUpdates.ps1
+++ PS/Invoke-NinjaGetUpdates.ps1
@@ -7,7 +7,7 @@ $WorkingDir = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows\Curr
 if (-not($WorkingDir)) {
     throw 'The NinjaGet installation directory could not be found in the registry. NinjaGet may not be properly installed.'   
 }
-$Functions = Get-ChildItem -Path (Join-Path -Path $WorkingDir -ChildPath 'PS') -Filter '*.ps1' -Exclude @('Send-NinjaGetNotification.ps1', 'Invoke-NinjaGetUpdate.ps1') -Recurse
+$Functions = Get-ChildItem -Path (Join-Path -Path $WorkingDir -ChildPath 'PS') -Filter '*.ps1' -Exclude @('Send-NinjaGetNotification.ps1', 'Invoke-NinjaGetUpdates.ps1') -Recurse
 foreach ($Function in $Functions) {
     Write-Verbose ('Importing function file: {0}' -f $Function.FullName)
     . $Function.FullName
